1703b68c2900584598d696631544fe9b463b754f
Tiago Seabra <tlgs@users.noreply.github.com>
2018-10-09 11:57:48 +0200

    updated 'Recursive' style

--- recursive/Find-the-downward-recommendation-chain-for-member-ID-1.sql
+++ recursive/Find-the-downward-recommendation-chain-for-member-ID-1.sql
@@ -1,14 +1,18 @@
 WITH RECURSIVE rec(memid) AS (
-		SELECT memid FROM cd.members WHERE recommendedby = 1
-	UNION ALL
-  		SELECT mem.memid
-  			FROM rec
-  				INNER JOIN cd.members AS mem
-  					ON mem.recommendedby = rec.memid
+    SELECT memid
+      FROM cd.members
+     WHERE recommendedby = 1
+
+     UNION ALL
+
+    SELECT mem.memid
+      FROM rec
+           INNER JOIN cd.members AS mem
+           ON mem.recommendedby = rec.memid
 )
 
 SELECT rec.memid, firstname, surname
-	FROM rec
-		INNER JOIN cd.members AS mem
-			ON rec.memid = mem.memid
-ORDER BY memid
+  FROM rec
+       INNER JOIN cd.members AS mem
+       ON rec.memid = mem.memid
+ ORDER BY memid;
