3c23732020b3a264bc18df364120c7e2c7a974a5
Tiago Seabra <tlgs@users.noreply.github.com>
2020-03-18 16:30:02 +0000

    fix: generate series with TIMESTAMP

--- 5-date/03-Generate-a-list-of-all-the-dates-in-October-2012.sql
+++ 5-date/03-Generate-a-list-of-all-the-dates-in-October-2012.sql
@@ -1,1 +1,1 @@
-SELECT generate_series(DATE '2012-10-01', DATE '2012-10-31', INTERVAL '1 day') AS ts;
+SELECT generate_series(TIMESTAMP '2012-10-01', TIMESTAMP '2012-10-31', INTERVAL '1 day') AS ts;
